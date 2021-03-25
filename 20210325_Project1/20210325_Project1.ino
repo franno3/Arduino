8aa46dfe8704337dafbfd610295a462c73f41a35
KamienKaze <HygrekRazuuk@gmail.com>
2021-03-25 16:27:25 +0100

    Wersja 1.1

--- 20210325_Project1/20210325_Project1.ino
+++ 20210325_Project1/20210325_Project1.ino
@@ -1,9 +1,15 @@
-void setup() {
-  // put your setup code here, to run once:
+//Stałe  
+const int buttonPin = A2;  //Przycisk
+const int ledPin = 13;     //Dioda
+//Zmienne
+int buttonPushCounter = 0; //Ilosc przycisniec
+int buttonState = 0;       //Aktualny stan przycisku
+int lastButtonState = 0;   //Poprzedni stan przycisku 
 
+void setup() {
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
